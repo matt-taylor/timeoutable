12b6d68dd6a3880aa4206e44924a450d92fd6f6e
Matt Taylor <mtaylor@chime.com>
2021-06-22 16:09:09 -0700

    first viable release

--- lib/timeoutable/version.rb
+++ lib/timeoutable/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Timeoutable
-  MAJOR = 0  # With backwards incompatability. Requires annoucment and update documentation
-  MINOR = 2  # With feature launch. Documentation of upgrade is useful via a changelog
-  PATCH = 1  # With minor upgrades or patcing a small bug. No changelog necessary
+  MAJOR = 1  # With backwards incompatability. Requires annoucment and update documentation
+  MINOR = 0  # With feature launch. Documentation of upgrade is useful via a changelog
+  PATCH = 0  # With minor upgrades or patcing a small bug. No changelog necessary
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.get_version
